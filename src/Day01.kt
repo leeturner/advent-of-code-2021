7110bac5b246adacc897d294cf79b5bdcbabd269
leeturnercrunch <lee.turner@crunch.co.uk>
2021-12-02 16:53:49 +0000

    LT - AOC day one solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        val pairs = input.windowed(size = 2)
+        return pairs.count { (a, b) -> a < b }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        val triplesCount = input.windowed(size = 3).map { it.sum()  }
+        val pairs = triplesCount.windowed(size = 2)
+        return pairs.count { (a, b) -> a < b }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01").map { it.toInt() }
+    println("Number of increases in the input part 1: ${part1(input)}")
+    println("Number of increases in the input part 2: ${part2(input)}")
 }
