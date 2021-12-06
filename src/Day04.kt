cb42907726edf053e802671c15d7d8087b41872b
Lee Turner <lee@leeturner.me>
2021-12-06 13:03:07 +0000

    LT - start of day 4

---
+++ src/Day04.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 198)
+//    check(part2(testInput) == 230)
+
+//    val input = readInput("Day03")
+//    println("The power consumption of the submarine?: ${part1(input)}")
+//    println("life support rating: ${part2(input)}")
+}
+
+
